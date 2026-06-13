cbdb527d8b71dd0b11137fbe0a6df12ec35a4262
Dexy Maroni <divineinyang7@gmail.com>
2026-06-13 05:53:38 +0100

    Restructure API URLs into auth, users, and drivers prefixes

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('users.urls')),
+    path('api/auth/', include('users.auth_urls')),
+    path('api/users/', include('users.profile_urls')),
+    path('api/drivers/', include('users.driver_urls')),
 ]
 
 if settings.DEBUG:
--- users/auth_urls.py
+++ users/auth_urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserProfileView, DriverProfileView
 from rest_framework_simplejwt.views import TokenRefreshView
+from .views import RegisterView, LoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
-    path('driver/profile/', DriverProfileView.as_view(), name='driver-profile'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
