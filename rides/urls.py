644ade0663491e2133f48191904167655120ddc2
Dexy Maroni <divineinyang7@gmail.com>
2026-06-16 13:04:19 +0100

    Add AvailableRidesView and AcceptRideView with race condition protection

--- rides/urls.py
+++ rides/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreateRideView
+from .views import AcceptRideView, CreateRideView, AvailableRidesView
 
 
 urlpatterns = [
-    path('request/', CreateRideView.as_view(), name='request-ride')
+    path('request/', CreateRideView.as_view(), name='request-ride'),
+    path('available/', AvailableRidesView.as_view(), name='available-rides'),
+    path('<int:ride_id>/accept/', AcceptRideView.as_view(), name='accept-ride'),
 ]
--- rides/views.py
+++ rides/views.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import CreateRideSerializer, RideSerializer
-from users.permissions import IsRider
+from users.permissions import IsRider, IsDriver
+from .models import Ride
 
 
 class CreateRideView(APIView):
@@ -14,3 +15,35 @@ class CreateRideView(APIView):
             ride =serializer.save(rider=request.user)
             return Response(RideSerializer(ride).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class AvailableRidesView(APIView):
+    permission_classes = [IsDriver]
+
+    def get(self, request):
+        rides = Ride.objects.filter(status=Ride.REQUESTED, driver=None)
+        serializer = RideSerializer(rides, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class AcceptRideView(APIView):
+    permission_classes = [IsDriver]
+
+    def patch(self, request, ride_id):
+        try:
+            ride = Ride.objects.get(id=ride_id)
+        except Ride.DoesNotExist:
+            return Response({"error": "Ride not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        # availability check (the if statement above)
+        if ride.status != Ride.REQUESTED or ride.driver is not None:
+            return Response({"error": "Ride is no longer available"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # update ride.driver, ride.status, save
+        ride.driver = request.user
+        ride.status = Ride.ACCEPTED
+        ride.save()
+
+
+        return Response(RideSerializer(ride).data, status=status.HTTP_200_OK)
